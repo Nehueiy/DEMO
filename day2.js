76e1a6fd46049943328e91012b3dc285043110a4
Nehueiy <nehacherry100@gmail.com>
2024-11-06 12:55:32 +0545

    learning variables

--- day2.js
+++ day2.js
@@ -1,1 +1,8 @@
 console.log("Hello world");
+let string = "Neha"
+var output = string[0]
+var next = string[3]
+console.log(output)
+console.log(next)
+let lastindex= string.length
+console.log(lastindex)
