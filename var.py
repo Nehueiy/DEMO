4a7f17b44568cfd5b80d02c2d3acb832888908f4
Nehueiy <nehacherry100@gmail.com>
2024-11-07 19:15:44 +0545

    kind of array

---
+++ var.py
@@ -0,0 +1,53 @@
+#height = 3
+#weight = 70
+#bmi= weight/height ** 2
+#print (bmi)
+x = "I am the best"
+y = 'We are the best'
+str
+print(x)
+monthly_savings = 10
+num_months = 4
+# Multiply monthly_savings and num_months
+new_savings = monthly_savings * num_months
+# Print new_savings
+print(new_savings)
+savings = 100
+new_savings = 40
+
+# Calculate total_savings using savings and new_savings
+total_savings = savings + new_savings
+print(total_savings)
+
+# Print the type of total_savings
+print(type(total_savings))
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Adapt list areas
+
+
+areas = ["hallway", hall, "kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath]
+
+# Print areas
+print(type(areas))
+print(areas[-10])
+print(areas[-1])
+print(areas[0])
+
+#Revision
+Height = 7 
+Weight = 70
+#Division
+Moight = Weight / Height
+print (Moight)
+x = "Know yourself"
+y = 'Concentration'
+str (print(y))
+print (type(x))
+fam=["Hi",9,"Li",8]
+print(fam[0])
+print(fam[-1])
